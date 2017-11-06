ac5502dcc7be76437c83ee2a5410f1c50013b123
rschramm <wobula@gmail.com>
2017-11-06 14:18:09 -0800

    Update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rschramm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 15:58:34 by rschramm          #+#    #+#             */
-/*   Updated: 2017/11/06 13:43:58 by rschramm         ###   ########.fr       */
+/*   Updated: 2017/11/06 14:06:23 by rschramm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 t_node	*head = NULL;
 
-t_data	*allocate_data(t_data *start, int capacity, size_t node_count)
+t_data	*allocate_data(t_data *start, int capacity, size_t node_count, int storage_size)
 {
 	t_data *tmp;
 
@@ -26,41 +26,44 @@ t_data	*allocate_data(t_data *start, int capacity, size_t node_count)
 	while (node_count > 0)
 	{
 		tmp->data = (void*)(tmp + 1);
-		tmp->next = (t_data*)((char*)tmp->data + tnysize);
+		tmp->next = (t_data*)((char*)tmp->data + storage_size);
 		node_count--;
-		capacity -= sizeof(t_data) + tnysize;
+		capacity -= (sizeof(t_data) + storage_size);
 		ft_printf("tny data    : %p\n", tmp->data);
 		ft_printf("tmp->next   : %p\n", tmp->next);
 		ft_printf("tny capacity: %d\n", capacity);
 		ft_printf("tny node cot: %d\n", node_count);
 		tmp = tmp->next;
 	}
-	return (tmp);	
+	return (start);	
+}
+
+t_node	*get_head()
+{
+	int get_size;
+	int data;
+
+	if (!head)
+	{
+		get_size = sizeof(t_node) + ((tnysize + sizeof(t_data)) * 100);
+		data = 0;
+		while (data < get_size)
+			data = data + getpagesize();
+		head = mmap(NULL, data, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+		head->available = data - (sizeof(t_node) + sizeof(t_data));
+		head->tny = (t_data*)(head + 1);
+		head->tny_size = (sizeof(t_data) + tnysize) * 100;
+		head->tny = allocate_data(head->tny, head->tny_size, 100, tnysize);
+	}
+	return(head);
 }
 
 void	 *malloc(size_t size)
 {
 	t_node *ptr;
-	int get_size;
-	int data;
-
-	get_size = sizeof(t_node) + ((tnysize + sizeof(t_data)) * 100);
-	data = 0;
-	while (data < get_size)
-		data = data + getpagesize();
-	ft_printf("get size: %d, page size: %d\n", get_size, getpagesize());
-	ft_printf("get this amount of data in page sizes: %d\n", data);
-	if (!head)
-	{
-		head = mmap(NULL, data, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-		head->size = size;
-		head->available = data - (sizeof(t_node) + sizeof(t_data));
-		head->tny = (t_data*)(head + 1);
-		head->tny_size = (sizeof(t_data) + tnysize) * 100;
-		head->tny = allocate_data(head->tny, head->tny_size, 100);
-		ptr = head;
-	}
-	ptr = head;
+	
+	ft_putnbr(size);
+	ptr = get_head();
 	return (ptr);
 }
 
