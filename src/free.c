b56cc5647ecdb9d5953d83cbb6ff52e29a18d92e
rschramm <wobula@gmail.com>
2017-11-07 08:45:32 -0800

    Added functionality for freeing big sized pointers.

--- src/free.c
+++ src/free.c
@@ -12,6 +12,28 @@
 
 #include "../includes/malloc.h"
 
+int		check_big_nodes(t_node *check, void *ptr)
+{
+	t_data *tmp;
+
+	if (!check->large)
+		return (0);
+	tmp = check->large;
+	while (tmp->next)
+	{
+		if (tmp->data == ptr && tmp->available == 0)
+			break ;
+		tmp = tmp->next;
+	}
+	if (tmp->data == ptr)
+	{
+		tmp->available = 1;
+		ft_printf("Freed pointer at address %p\n", tmp->data);
+		return (1);
+	}
+	return (0);
+}
+
 void	 free(void *ptr)
 {
 	t_node *tmp;
@@ -20,6 +42,8 @@ void	 free(void *ptr)
 
 	ft_printf("freeing ptr address: %p\n", ptr);
 	tmp = get_head();
+	if (check_big_nodes(tmp, ptr))
+		return ;
 	if (ptr < (void*)tmp->tny_end)
 	{
 		find = tmp->tny;
