7eba54d8fd78569f8eb2a1406f2e1923d887acc1
rschramm <wobula@gmail.com>
2017-11-14 14:24:55 -0800

    Update to main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rschramm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 15:58:34 by rschramm          #+#    #+#             */
-/*   Updated: 2017/11/13 17:55:08 by rschramm         ###   ########.fr       */
+/*   Updated: 2017/11/14 14:24:14 by rschramm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,31 @@ int		main(void)
 {
 	char	**ptr;
 	int		x;
+	int		y;
 
 	while (1)
 	{
-		ptr = (char**)calloc(sizeof(void *), 200);
+		ptr = (char**)calloc(sizeof(char *), 200);
+		x = -1;
+		y = 0;
+		while (x++ < 200)
+		{
+			y = -1;
+			ptr[x] = (char*)malloc(10);
+			while (++y < 8)
+				ptr[x][y] = 'a';
+			ptr[x][y] = '\0';
+			ft_dprintf(2, "Malloc string: %s\n", ptr[x]);
+			ptr[x] = (char*)realloc(ptr[x], 300);
+			y = -1;
+			while (++y < 298)
+				ptr[x][y] = 'b';
+			ptr[x][y] = '\0';
+			ft_dprintf(2, "realloc string: %s\n", ptr[x]);
+		}
 		x = -1;
 		while (x++ < 200)
 		{
-			ptr[x] = (char*)malloc(10);
-			ptr[x][0] = 'a';
-			ft_dprintf(2, "Malloc char: %c\n", ptr[x][0]);
-			ptr[x] = (char*)realloc(ptr[x], 300);
-			ptr[x][0] = 'b';
-			ft_dprintf(2, "Malloc char: %c\n", ptr[x][0]);
 			free(ptr[x]);
 		}
 		free(ptr);
